6e60b70b772fdd292f9dedf61d0b152eef4d69e2
Kirill Gadjello <111190790+kir-gadjello@users.noreply.github.com>
2023-10-25 11:40:09 -0300

    infra updates

---
+++ rebuild_native_libs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cd ../llama.cpp;
+rm -rf build;
+mkdir build;
+cd build;
+rm -rf .;
+cmake .. -DBUILD_SHARED_LIBS=1 -DLLAMA_METAL=1
+cmake --build . --config Release --target rpcserver server_oaicompat;
+cd ..;
+
+
+cp ./build/examples/server/librpcserver.dylib ../handheld_helper/native/
+cp ./examples/server/rpcserver.h ../handheld_helper/native/
+cp ./examples/server/rpcserver.h ../handheld_helper/
+cp ./build/libllama.dylib ../handheld_helper/native/
+cp ./ggml-metal.metal ../handheld_helper/native/
+
+cd ../handheld_helper;
+cp native/ggml-metal.metal macos/Runner/
+cp native/*.dylib macos/Runner/
+
+flutter pub run ffigen;
